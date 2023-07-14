6191320687836c2d89205eaea1a60718ce5d9c51
Andy Smith <andyrsmith@gmail.com>
2023-07-14 11:36:28 +0100

    Initial commit.

---
+++ app/src/main/kotlin/kotlin_experiment9/app/App.kt
@@ -0,0 +1,24 @@
+package kotlin_experiment9.app
+
+import kotlin_experiment9.utilities.StringUtils
+
+import org.apache.commons.text.WordUtils
+
+fun twice(): String {
+    return "Foo"
+}
+
+data class Person( val first : String, val last : String)
+
+fun main() {
+    val tokens = StringUtils.split(MessageUtils.getMessage())
+    val result = StringUtils.join(tokens)
+    println(WordUtils.capitalize(result))
+    println(twice())
+
+    val xs = Person("andy","smith")
+
+    val (first, last) = xs
+
+    print("Name is $first - $last")
+}
